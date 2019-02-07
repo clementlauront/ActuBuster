3d50b573d9d26d54e7c62ba2c3cee0b1db2fe8cc
clementlauront <clement.lauront@gmail.com>
2019-02-07 09:30:57 +0100

    test nul

--- src/main/java/servlets/Accueil.java
+++ src/main/java/servlets/Accueil.java
@@ -26,6 +26,7 @@ public class Accueil extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HAHAHA
 	}
 
 	/**
