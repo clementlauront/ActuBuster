a6b21f1b3e595ef755c745213bd0a92f421917c3
SirAGrondin <alex.dibuchle@gmail.com>
2019-02-11 17:01:07 +0100

    MAJ-servlet connexion

--- src/main/java/servlets/Connexion.java
+++ src/main/java/servlets/Connexion.java
@@ -1,11 +1,17 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import beans.Membres;
 
@@ -28,7 +34,23 @@ public class Connexion extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String mdp = request.getParameter("mdp");
+		String messageErreur ="";
 		
+		SessionFactory usine = new Configuration().configure().buildSessionFactory();
+		Session gestionnaire = usine.openSession();
+		
+		@SuppressWarnings("unchecked")
+		List<Membres> resultat = (List<Membres>) gestionnaire.createQuery("from Membres WHERE email = "+email+" & PASSWORD = "+mdp);
+		if(resultat.size() == 0) {
+			messageErreur = "L'email ou le mot de passe que vous avez inscrit est incorrect.";
+			//TODO Faire apparaitre message d'erreur
+		}else if(resultat.size()==1) {
+			HttpSession session = request.getSession(false) ;
+			response.sendRedirect("/PageMembre");
+
+		}
 	}
 
 	/**
