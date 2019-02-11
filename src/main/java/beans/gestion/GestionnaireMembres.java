04667e66043c6d65e04d2bf810042ebf4e10d839
clementlauront <clement.lauront@gmail.com>
2019-02-11 14:51:54 +0100

    gestionnaire

--- src/main/java/beans/gestion/GestionnaireMembres.java
+++ src/main/java/beans/gestion/GestionnaireMembres.java
@@ -1,9 +1,62 @@
 package beans.gestion;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import beans.Articles;
+import beans.Membres;
+
 public class GestionnaireMembres {
+	SessionFactory sessionFactory;
 
 	public GestionnaireMembres() {
-		// TODO Auto-generated constructor stub
+		this.sessionFactory = new Configuration().configure().buildSessionFactory();
+	}
+	
+	public boolean addMembre(Membres membre) {
+		// TODO prévoir si l'ajout ne se fait pas
+		Session session = this.sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(membre);
+		session.getTransaction().commit();
+		session.close();
+		return true;
+	}
+	
+	public boolean deleteMembreById(int id) {
+		// TODO prévoir si id n'existe pas
+		Session session = this.sessionFactory.openSession();
+		session.beginTransaction();
+		Membres membre = (Membres) session.get(Membres.class, id);
+		session.getTransaction().commit();
+		session.beginTransaction();
+		session.delete(membre);
+		session.getTransaction().commit();
+		session.close();
+		return true;
+	}
+	
+	public boolean updateMembre(int id) {
+		// TODO prévoir si id de l'article n'existe pas
+		Session session = this.sessionFactory.openSession();
+		session.beginTransaction();
+		Membres membre = (Membres) session.get(Membres.class, id);
+		session.getTransaction().commit();
+		session.beginTransaction();
+		session.update(membre);
+		session.getTransaction().commit();
+		session.close();
+		return true;
+	}
+	
+	public Membres getMembreById(int id) {
+		Session session = this.sessionFactory.openSession();
+		session.beginTransaction();
+		Membres membre = (Membres) session.get(Membres.class, id);
+		session.getTransaction().commit();
+		session.close();
+		return membre;
 	}
 
 }
