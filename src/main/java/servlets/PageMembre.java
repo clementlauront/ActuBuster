622cb7dd9f9a1ed86667d173f3831a8db6ce7713
SirAGrondin <alex.dibuchle@gmail.com>
2019-02-12 15:59:25 +0100

    MAJ-Blabla

--- src/main/java/servlets/PageMembre.java
+++ src/main/java/servlets/PageMembre.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class PageMembre
@@ -34,7 +35,15 @@ public class PageMembre extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		// Récupérer les informations du formulaire
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String password = request.getParameter("mdp");
+		
+		// Récupérer la session
+				HttpSession session = request.getSession(false);
 		// récupérer le formulaire, comparer à la liste des membres, si email ou pseudo ou (prénom et nom) correspond à un autre membre, renvoyer erreur
 		// sinon, modifier les informations du loggé et le renvoyer vers espace membre
 		
