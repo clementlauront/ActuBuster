618c77ab4f4548d94eb861271aa0a2a58e59f9a1
clementlauront <clement.lauront@gmail.com>
2019-02-12 15:59:21 +0100

    administration servlet fini

--- src/main/java/servlets/AdministrationDuSite.java
+++ src/main/java/servlets/AdministrationDuSite.java
@@ -1,40 +1,93 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.Articles;
+import beans.Membres;
+import beans.Tags;
+import beans.gestion.GestionnaireArticle;
+import beans.gestion.GestionnaireMembres;
+import enumerations.Categories;
+import enumerations.Niveaux;
 
 /**
  * Servlet implementation class AdministrationDuSite
  */
 public class AdministrationDuSite extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdministrationDuSite() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// récupérer la liste des articles et des membres, permettre de supprimer les articles et les membres qu'on veut
-		
-		this.getServletContext().getRequestDispatcher("/WEB-INF/pageAdmoinistrationDuSite/index.jsp").forward(request, response);
+	public AdministrationDuSite() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+
+		if (session != null) {
+			Membres loggeur = (Membres) session.getAttribute("LOGGEUR");
+			if (loggeur.getNiveaux() == Niveaux.ADMIN) { // Si une session admin existe, on donne accès à la page
+
+				// on transmet les listes de membres et d'articles
+				GestionnaireArticle gestA = new GestionnaireArticle();
+				GestionnaireMembres gestM = new GestionnaireMembres();
+				ArrayList<Membres> membres = gestM.getAllMembres();
+				ArrayList<Articles> articles = gestA.getAllArticles();
+				request.setAttribute("listeMembres", membres);
+				request.setAttribute("listeArticles", articles);
+
+				this.getServletContext().getRequestDispatcher("/WEB-INF/pageAdministrationDuSite/index.jsp")
+						.forward(request, response);
+
+			} else { // Sinon, on affiche la page d'acceuil
+				response.sendRedirect("/ActuBuster/Accueil");
+			}
+		} else {
+			response.sendRedirect("/ActuBuster/Accueil");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// instancier les variables temporaires
+		GestionnaireArticle gestA = new GestionnaireArticle();
+		GestionnaireMembres gestM = new GestionnaireMembres();
+
+		// récupérer le formulaire
+		String[] idArticlesASupprimer = request.getParameterValues("ArticlesASupprimer");
+		String[] idMembresASupprimer = request.getParameterValues("membresASupprimer");
+
+		// supprimer les articles à supprimer
+		for (int i = 0; i < idArticlesASupprimer.length; i++) {
+			gestA.deleteArticleById(Integer.parseInt(idArticlesASupprimer[i]));
+		}
+
+		// supprimer les membres à supprimer
+		for (int i = 0; i < idMembresASupprimer.length; i++) {
+			gestM.deleteMembreById(Integer.parseInt(idMembresASupprimer[i]));
+		}
+
 		doGet(request, response);
 	}
 
--- src/main/webapp/WEB-INF/pageAdministrationDuSite/index.jsp
+++ src/main/webapp/WEB-INF/pageAdministrationDuSite/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<p>Ha ha ha</p>
 </body>
 </html>
