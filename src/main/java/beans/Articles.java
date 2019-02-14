0c44eea68de5d8458aa49ff6aa343e02eeb007fb
SirAGrondin <alex.dibuchle@gmail.com>
2019-02-14 10:40:53 +0100

    MAj-BDD

--- src/main/java/beans/Articles.java
+++ src/main/java/beans/Articles.java
@@ -5,13 +5,11 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import enumerations.Categories;
 
@@ -30,10 +28,10 @@ public class Articles {
 	@Column
 	private Categories categorie;
 	
-	@Column(name="CONTENUE")
+	@Column(name="CONTENUE", columnDefinition="TEXT")
 	private String contenue;
 	
-	@OneToMany(cascade= {CascadeType.ALL}, orphanRemoval=true)
+	@ManyToMany(cascade= {CascadeType.ALL})
 	private List<Tags> tags;
 	
 	@Column(name="NOMBREVUES")
